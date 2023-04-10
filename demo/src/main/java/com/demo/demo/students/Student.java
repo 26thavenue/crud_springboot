083b7233646d3e6bef18143e88dc0c75a757d398
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2023-04-10 23:34:19 +0100

    Added some new files

--- demo/src/main/java/com/demo/demo/students/Student.java
+++ demo/src/main/java/com/demo/demo/students/Student.java
@@ -1,10 +1,26 @@
 package com.demo.demo.students;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
+
+    @Transient
     private Integer age;
     private LocalDate dob;
     private String email;
@@ -12,17 +28,15 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String name, Integer age, LocalDate dob, String email) {
+    public Student(Long id, String name, LocalDate dob, String email) {
         this.id = id;
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
 
-    public Student(String name, Integer age, LocalDate dob, String email) {
+    public Student(String name, LocalDate dob, String email) {
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
--- demo/src/main/java/com/demo/demo/students/StudentService.java
+++ demo/src/main/java/com/demo/demo/students/StudentService.java
@@ -1,6 +1,6 @@
 package com.demo.demo.students;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,15 +10,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Naomi",
-                        24,
-                        LocalDate.of(2000, Month.FEBRUARY, 5),
-                        "naomi@gmail.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
